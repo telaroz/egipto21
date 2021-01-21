e063559fce9d0d6c6400bd2d15079178d9c806ea
Valery Zúñiga Kondrashov <telaroz@gmail.com>
2021-01-21 15:54:41 -0600

    Agregar scrape_from_ihf

--- R/utilidades.R
+++ R/utilidades.R
@@ -15,3 +15,22 @@ llenar_nas <- function(tabla, columna){
 
   tabla[is.na(get(columna)), (columna) := primer_no_na]
 }
+
+
+#' Función para descargar pdfs.
+#'
+#' @param enlace Página desde donde queremos descargar los partidos
+#'
+#' @return
+#' @export
+#'
+#' @examples scrape_from_ihf(enlace = 'https://www.ihf.info/competitions/men/308/27th-ihf-men039s-world-championship-2021/22415/match-center/23765', carpeta = 'matches')
+scrape_from_ihf <- function(enlace, carpeta){
+  pagina %>%
+    xml2::read_html() %>%
+    rvest::html_nodes("a") %>%
+    rvest::html_attr("href") %>%
+    stringr::str_subset("\\.pdf|\\.PDF") %>%
+    purrr::walk2(., paste0(carpeta,'/', basename(.) %>% stringr::str_remove('[?=].*')), download.file, mode = "wb")
+
+}
